f50eaa926db3aeb2486a74872f23c51784b11dd7
Mohamed Bakli <44028512+mbakli@users.noreply.github.com>
2020-02-25 10:03:26 +0100

    Update AIS_queries.sql

--- AIS_queries.sql
+++ AIS_queries.sql
@@ -24,20 +24,22 @@
 	AND Trip && Period('2019-09-01', '2019-09-30')
 	AND timespan(Trip) > '5 days';
 -------------------------------------------------------------------------------
--- Query 4: What is the total travelled distance of all trips between the ports of Rødby and Puttgarden in Sept 2, 19?
-	SELECT Ship_id, length(Trip)
-	FROM Ships
-	WHERE Destination='Puttgarden'
-	AND Trip && Period('2019-09-02 00:00:00', '2019-09-02 23:59:59');
--------------------------------------------------------------------------------
---Query 5: What are the ships that have passed a specific area in the sea?
-	SELECT Ship_id
-	FROM Ships
-	WHERE intersects(Trip, ST_MakeEnvelope(640730, 6058230, 654100, 6042487, 25832));
--------------------------------------------------------------------------------
---Query 6: List the minimum distance ever between each ship and a specific point
+--Query 4: List the minimum distance ever between each ship and a specific point
 	SELECT Ship_id, MIN(trajectory(Trip) <-> ST_transform(ST_setsrid(ST_MakePoint(10.945873, 56.447255), 4326), 25832)) AS MinDistance
 	FROM Ships_dist 
 	GROUP BY mmsi
 	ORDER BY mmsi;
+
+-------------------------------------------------------------------------------
+-- Query 5: What is the total travelled distance of all trips between the ports of Rødby and Puttgarden in Sept 2, 19?
+	SELECT Ship_id, length(Trip)
+	FROM Ships
+	WHERE Destination='Puttgarden'
+	AND Trip && Period('2019-09-02 00:00:00', '2019-09-02 23:59:59');
+
+-------------------------------------------------------------------------------
+--Query 6: What are the ships that have passed a specific area in the sea?
+	SELECT Ship_id
+	FROM Ships
+	WHERE intersects(Trip, ST_MakeEnvelope(640730, 6058230, 654100, 6042487, 25832));
 -------------------------------------------------------------------------------
